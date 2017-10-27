25c77dd1dc9e951c47c1c02460198f8bd9ec1480
Wissam-G <wissam.ghozlan@gmail.com>
2017-10-27 18:02:15 +0200

    change headline style

--- js/main.js
+++ js/main.js
@@ -102,3 +102,5 @@ var TxtType = function(el, toRotate, period) {
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+
+    
